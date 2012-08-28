f32acc484e8cc05ff4e7bef5233e5410e5b3dd94
Ryan G Hunter <rghunter@bu.edu>
2012-08-27 22:20:42 -0400

    added freak descriptors

--- src/gstOpticalProcessor.cc
+++ src/gstOpticalProcessor.cc
@@ -5,7 +5,11 @@
  *      Author: rghunter
  */
 
-#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/legacy/legacy.hpp>
 #include "gstopticalquad.h"
 #include <iostream>
 
@@ -16,28 +20,21 @@ using namespace std;
 using namespace cv;
 
 deque<Mat> frame_buffer;
-bool unDistort;
-Mat outgoing_frame, frame_gray, save_image;
+bool unDistort, first;
+Mat outgoing_frame, frame_gray, save_image, fixed_image;
 Mat frame_corrected;
+Mat fixed_descriptor;
 Mat cameraMatrix, distCoeffs;
-Mat corners[2];
-int corner_count;
-TermCriteria subPixCriteria;
-Size subPixWindowSize;
+
+vector<KeyPoint> keypoint_fixed;
 
 
 gboolean init_frameprocessor(int width, int height)
 {
-	frame_buffer.clear();
-	corner_count = MAX_CORNERS;
-	subPixWindowSize = Size(WINDOW_SIZE,WINDOW_SIZE);
-
-	subPixCriteria.epsilon = 0.3;
-	subPixCriteria.maxCount = 20;
-	subPixCriteria.type = (CV_TERMCRIT_ITER | CV_TERMCRIT_EPS);
 
 	frame_gray.create(width,height,CV_8UC1);
 	frame_corrected.create(width,height,CV_8UC3);
+	first = true;
 
 	FileStorage fs("calib.xml",FileStorage::READ);
 	if(!fs.isOpened())
@@ -55,70 +52,38 @@ gboolean init_frameprocessor(int width, int height)
 		unDistort = true;
 	}
 	cout << "Called init_frameprocessor function" << endl;
+
 	return TRUE;
 }
 void process_frame(IplImage *input, IplImage *output)
 {
-
+	SurfFeatureDetector detector(1000,8);
+	FREAK extractor;
 	Mat raw_frame(input);
+	BruteForceMatcher <Hamming> matcher;
+	vector<KeyPoint> keypoint;
+	Mat descriptor;
+	Mat output_frame;
+	vector<DMatch> matches;
 
-	Mat status, errors;
-
-	if(unDistort)
+	if(first)
 	{
-		undistort(raw_frame,frame_corrected,cameraMatrix,distCoeffs);
-	}else{
-		raw_frame.copyTo(frame_corrected);
-	}
-
-	Size video_frame_size = raw_frame.size();
-
-	cvtColor(frame_corrected,frame_gray, CV_RGB2GRAY);
-
-	frame_buffer.push_front(frame_gray.clone());
-
-	if((int)frame_buffer.size() > HISTORY){
-		frame_buffer.pop_back();
-	}else{
+		fixed_image = raw_frame.clone();
+		detector.detect(raw_frame,keypoint_fixed);
+		extractor.compute(raw_frame,keypoint_fixed,fixed_descriptor);
+		first = false;
 		return;
 	}
 
-	goodFeaturesToTrack(frame_buffer[0],corners[0],corner_count,
-						0.01, //quality
-						10.0); //min seperation
+	detector.detect(raw_frame,keypoint);
+	extractor.compute(raw_frame,keypoint,descriptor);
+	matcher.match(fixed_descriptor,descriptor,matches);
+	//drawMatches(raw_frame,keypoint_fixed,raw_frame,keypoint,matches,output_frame);
+	//imshow("output",output_frame);
+	drawKeypoints(raw_frame,keypoint,raw_frame,Scalar(255,0,0));
+	drawKeypoints(raw_frame,keypoint_fixed,raw_frame,Scalar(0,255,0),DrawMatchesFlags::DRAW_OVER_OUTIMG);
 
-	cornerSubPix(frame_buffer[0],corners[0],subPixWindowSize,Size(-1,-1),subPixCriteria);
-
-
-
-	calcOpticalFlowPyrLK(frame_buffer[0],frame_buffer[1],corners[0],corners[1],status, errors);
-
-#if 1
-	Mat vectors = corners[0] - corners[1];
-	float vel = 0;
-	float x = 0;
-	float y = 0;
-	int len = 0;
-
-	for(MatConstIterator_<Point2f> velocity_vector = vectors.begin<Point2f>(); velocity_vector != vectors.end<Point2f>(); ++velocity_vector)
-	{
-		vel += sqrt(pow((*velocity_vector).x,2)+pow((*velocity_vector).y,2));
-		x += (*velocity_vector).x / vel;
-		y += (*velocity_vector).y / vel;
-		len++;
-	}
-
-	vel = vel / (float)len;
-	y = y / (float)len;
-	x = x / (float)len;
-
-	Point start(video_frame_size.width/2,video_frame_size.height/2);
-	line(frame_corrected,start,Point(video_frame_size.width/2+(x*vel*10.0),video_frame_size.height/2+(y*vel*10.0)),Scalar(255,0,0),3);
-
-//	cout << y << x << endl;
-#endif
-
-	*output = frame_corrected;
+	*output = raw_frame;
 
 	//memcpy(output->imageData,input->imageData,output->imageSize);
 	return;
